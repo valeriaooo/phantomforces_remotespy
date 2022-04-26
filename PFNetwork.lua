f67b09db851c0571bfcc77dace96d43d73252ee4
philip <86986414+philipdeva@users.noreply.github.com>
2022-04-26 20:06:50 +0100

    due to patch by devs, changes.

--- PFNetwork.lua
+++ PFNetwork.lua
@@ -1,48 +1,83 @@
 -- original code from https://v3rmillion.net/showthread.php?tid=974599
-
 local players = game:GetService('Players');
 local client = players.LocalPlayer;
 
-local network = require(game:GetService("ReplicatedFirst").ClientModules.Old.framework.network)
-local send = network.send;
-local fetch = network.fetch;
+local network
+local old_send
+local old_fetch
 
-getgenv().filter = {"ping", "repupdate", "suppressionassist"}
-
-function contains(table, element)
-    for _, value in pairs(table) do
-      if value == element then
-        return true
-      end
+for i, v in next, getgc(true) do
+    if type(v) == "function" and not is_synapse_function(v) then
+        for k, x in next, debug.getupvalues(v) do
+            if type(x) == "table" then
+                if rawget(x, 'send') then
+                    network = x
+                    old_send = x.send
+                    old_fetch = x.fetch
+                end
+            end
+        end
     end
-    return false
-  end
+end
+
+getgenv().filter = {"ng", "repup", "ressionassist"}
+
+local function stringcontains(str, sub)
+    return string.find(str, sub) ~= nil
+end
+
+local function LookThroughAllTables(table)
+    if type(table) == "table" then
+        warn(("="):rep(1), tostring(table), ("="):rep(1))
+        for i, v in next, table do
+            print(i, v)
+            if type(v) == "table" then
+                LookThroughAllTables(v)
+            end
+        end
+    end
+end
 
 function network.send(self, action, ...)
     local arguments = {...}
-    if contains(getgenv().filter, action) then return send(self, action, unpack(arguments)) end
+    if stringcontains(action, "repup") or stringcontains(action, "ressionassist") or stringcontains(action, "ng") then
+        return old_send(self, action, unpack(arguments))
+    end
 
     print(("="):rep(10))
     print('network.send called with action:', action)
-    
+
     for i, v in next, arguments do
         warn(i, v)
+        if type(v) == "table" then
+            LookThroughAllTables(v)
+        end
     end
-    
+
     print(("="):rep(10))
-    return send(self, action, unpack(arguments))
+    return old_send(self, action, unpack(arguments))
 end
+
+-- do for loops until type is no longer a table
 
 function network.fetch(self, action, ...)
     local arguments = {...}
-    if contains(getgenv().filter, action) then return send(self, action, unpack(arguments)) end
+    if stringcontains(action, "repup") or stringcontains(action, "ressionassist") or stringcontains(action, "ng") then
+        return old_fetch(self, action, unpack(arguments))
+    end
+
     print(("="):rep(10))
     print('network.fetch called with action:', action)
-        
+
     for i, v in next, arguments do
-            warn(i, v)
+        warn(i, v)
+        if type(v) == "table" then
+            if type(v) == "table" then
+                LookThroughAllTables(v)
+            end
         end
-    
-        print(("="):rep(10))
-    return fetch(self, action, unpack(arguments))
+    end
+
+    print(("="):rep(10))
+    return old_fetch(self, action, unpack(arguments))
 end
