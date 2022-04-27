2ea03cc794b4c5a2f68f91a83f6b5ac988dcf6ad
philip <86986414+philipdeva@users.noreply.github.com>
2022-04-27 01:37:57 +0100

    replaces the whitespaces in action with underscore

--- PFNetwork.lua
+++ PFNetwork.lua
@@ -22,13 +22,40 @@ end
 
 getgenv().filter = {"ng", "repup", "ressionassist"}
 
-local function stringcontains(str, sub)
+local function StringContans(str, sub)
     return string.find(str, sub) ~= nil
+end
+
+-- check if character is in the alphabet 
+local function IsAlphabet(char)
+    return char:match("%a") ~= nil
+end
+
+-- loop through all characters in a string
+local function ForEachChar(str, func)
+    for i = 1, #str do
+        func(str:sub(i, i))
+    end
+end
+
+
+-- loop through each character in the string#
+-- check for a whitespace and replace it with a underscore
+local function ReplaceWhitespace(str)
+    local new_str = ""
+    ForEachChar(str, function(char)
+        if not IsAlphabet(char) then
+            new_str = new_str .. "_"
+        else
+            new_str = new_str .. char
+        end
+    end)
+    return new_str
 end
 
 local function LookThroughAllTables(table)
     if type(table) == "table" then
-        warn(("="):rep(1), tostring(table), ("="):rep(1))
+        warn(("="):rep(2), tostring(table), ("="):rep(2))
         for i, v in next, table do
             print(i, v)
             if type(v) == "table" then
@@ -36,16 +63,18 @@ local function LookThroughAllTables(table)
             end
         end
     end
+    warn(("="):rep(2), tostring(table), ("="):rep(2))
 end
 
 function network.send(self, action, ...)
     local arguments = {...}
-    if stringcontains(action, "repup") or stringcontains(action, "ressionassist") or stringcontains(action, "ng") then
+    if StringContans(action, "repup") or StringContans(action, "ressionassist") or StringContans(action, "ng") then
         return old_send(self, action, unpack(arguments))
     end
 
     print(("="):rep(10))
     print('network.send called with action:', action)
+    warn('action whitespaces:', ReplaceWhitespace(action))
 
     for i, v in next, arguments do
         warn(i, v)
@@ -62,12 +91,13 @@ end
 
 function network.fetch(self, action, ...)
     local arguments = {...}
-    if stringcontains(action, "repup") or stringcontains(action, "ressionassist") or stringcontains(action, "ng") then
+    if StringContans(action, "repup") or StringContans(action, "ressionassist") or StringContans(action, "ng") then
         return old_fetch(self, action, unpack(arguments))
     end
 
     print(("="):rep(10))
     print('network.fetch called with action:', action)
+    warn('action whitespaces:', ReplaceWhitespace(action))
 
     for i, v in next, arguments do
         warn(i, v)
